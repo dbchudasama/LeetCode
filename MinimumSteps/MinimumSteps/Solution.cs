9dfe8fee8f4707421d17750b4a431f988d1fa70b
Divyesh Chudasama <divyeshchudasama@yahoo.co.uk>
2021-04-03 20:47:13 +0100

    Minimum Number of Steps needed to make pile Heights Equal completed

---
+++ MinimumSteps/MinimumSteps/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace MinimumSteps
+{
+    class Solution
+    {
+
+
+        /// <summary>
+        /// Method to calculate minimum number of steps to make pile heights equal
+        /// </summary>
+        /// <param name="pilesHeight"></param>
+        /// <returns>Number of Steps</returns>
+        public int minStepsToMakePilesHeightEqual(int[] pilesHeight)
+        {
+
+            //Reverse Array -> Largest to smallest
+            Array.Reverse(pilesHeight); 
+
+            //Counters
+            int steps = 0;
+            int i = 1;
+
+
+            //While i is less than the length of the pile
+            while(i < pilesHeight.Length)
+            {
+                //If the i'th item is not equal to it's preceding item
+                if (pilesHeight[i] != pilesHeight[i - 1])
+                {
+                    //steps = steps + i
+                    steps += i;
+                }
+                //Increment i
+                i++;
+            }
+
+            return steps;
+            //O(NlognN)
+        }
+
+
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var piles = new int[] { 1, 3, 8 };
+            var steps = s.minStepsToMakePilesHeightEqual(piles);
+
+            Console.WriteLine($"The Minimum number of steps needed to equal pile heights ({String.Join(",", piles.Select(x => x.ToString()).ToArray())}) is {steps}");
+        }
+    }
+}
